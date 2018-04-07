327c3f70b4a8b20e9d9845de6a41cae369d01670
github username <github email address>
2018-04-06 21:23:24 -0700

    added fetch, componentDidMount, and constructor of behaviors

--- src/container/About.js
+++ src/container/About.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import { API_URL } from '../actions/apiUrl'
+import Behaviors from '../components/users/behaviors/Behaviors';
 
 class About extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      behaviors: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${API_URL}/behaviors`)
+     .then(res => res.json())
+     .then(behaviors => this.setState({ behaviors }))
+  }
+
   render() {
     return (
       <div className="bodyContainer">
@@ -8,6 +23,7 @@ class About extends Component  {
           <h1 className="App-title">About Self-Care</h1>
           <p className="App-intro">Self-Care tracks daily habits that can help manage symptoms for those affect by Mental Health. Tracking these daily routines and habits can help inform the user on possible trigger points and mood changes. Setting a daily routine and creating a pattern of behaviors can help improve symptoms in hopes for a brighter future. Below are some of the habits and routines we include for tracking.</p>
         </div>
+        <Behaviors behaviors={this.state.behaviors} />
       </div>
     )
   }
