ea9988b71380cd8e333cfea1193540d12477397f
github username <github email address>
2018-04-08 11:53:02 -0700

    added placeholder code for future reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,18 @@
 import { combineReducers } from 'redux';
+import authReducers from './authReducers';
+import medicationReducers from './medicationReducers';
+// import insuranceReducers from './insuranceReducers';
+// import providerReducers from './providerReducers';
+// import behaviorReducers from './behaviorReducers';
+// import conditionReducers from './conditionReducers';
 
 const rootReducer = combineReducers({
-
+  auth: authReducers,
+  medication: medicationReducers,
+  // insurance: insuranceReducers,
+  // provider: providerReducers,
+  // behavior: behaviorReducers,
+  // condition: conditionReducers
 });
 
 export default rootReducer;
