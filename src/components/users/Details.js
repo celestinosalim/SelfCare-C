e7b6a7cd5660869c8afc50b55d95743fb09e981a
github username <github email address>
2018-04-08 22:06:19 -0700

    updated the user Name heading to be the users name

--- src/components/users/Details.js
+++ src/components/users/Details.js
@@ -4,10 +4,9 @@ const Details = ({user}) => {
   return (
     <div className="UserDetails">
       <div className="AttrTitle">
-        <h1>User Name</h1>
+        <h1>{user.name}</h1>
       </div>
       <div className='AttrInfo' key={user.id}>
-        <p>Name: {user.name}</p>
         <p>Email: {user.email}</p>
       </div>
     </div>
