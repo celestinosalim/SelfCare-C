b51d6b84466a5ce92752f3100b65d3c8f2258164
github username <github email address>
2018-04-09 13:49:22 -0700

    cleaned up file; added action creators and async actions

--- src/actions/medicationActions.js
+++ src/actions/medicationActions.js
@@ -1,10 +1,43 @@
+import axios from 'axios'
 import { API_URL } from './apiUrl'
 import * as types from './actionTypes'
 
-export function addMedications(){
-  const request = fetch(`${API_URL}/medications`)
+// Action Creators
+const setMedications = medications => {
+  return {
+    type: types.REQUEST_MEDICATIONS,
+    medications
+  }
+}
+
+const addMedication = medication => {
   return {
     type: types.ADD_MEDICATION,
-    payload: request
+    medication
   }
+}
+
+// Async Actions
+export const getMedications = () => {
+  return dispatch => {
+    return fetch(`${API_URL}/medications`)
+      .then(response => response.json())
+      .then(medications => dispatch(setMedications(medications)))
+      .catch(error => console.log(error));
+  };
+}
+
+export const createMedications = medication => {
+  return dispatch => {
+    return fetch(`${API_URL}/medications`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({medication: medication})
+    })
+      .then(response => response.json())
+      .then(medication => dispatch(addMedication(medication)))
+      .catch(error => console.log(error))
+  };
 }
