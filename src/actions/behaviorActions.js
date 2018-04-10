9a02cf5d7030994d298ed6a8ae8a337faca6508a
github username <github email address>
2018-04-09 19:57:10 -0700

    updated actions

--- src/actions/behaviorActions.js
+++ src/actions/behaviorActions.js
@@ -1,24 +1,48 @@
 import { API_URL } from './apiUrl'
 import * as types from './actionTypes'
 
-export function fetchBehaviors(){
-  return (dispatch) => {
-    dispatch({
-      type: types.REQUEST_BEHAVIORS
-    });
+// Action Creators
+const setBehaviors = behaviors => {
+  return {
+    type: types.REQUEST_BEHAVIORS,
+    behaviors
+  }
+}
+
+const addBehavior = behavior => {
+  return {
+    type: types.CREATE_BEHAVIOR,
+    behavior
+  }
+}
+
+export const updateBehaviorFormData = behaviorFormData => {
+  return {
+    type: types.UPDATE_BEHAVIOR,
+    behaviorFormData
+  }
+}
+
+export const getBehaviors = () => {
+  return dispatch => {
     return fetch(`${API_URL}/behaviors`)
       .then(response => response.json())
-      .then(behaviors => dispatch({
-        type: types.ADD_BEHAVIORS,
-        behaviors
-      }))
+      .then(behaviors => dispatch(setBehaviors(behaviors)))
+      .catch(error => console.log(error));
   };
 }
 
-export function createBehaviors(){
-
-  return {
-    type: types.CREATE_BEHAVIORS,
-    behaviors
+export const createBehavior = behavior => {
+  return dispatch => {
+    return fetch(`${API_URL}/behaviors`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({behavior: behavior})
+    })
+      .then(response => response.json())
+      .then(behavior => dispatch(addBehavior(behavior)))
+      .catch(error => console.log(error))
   };
 }
