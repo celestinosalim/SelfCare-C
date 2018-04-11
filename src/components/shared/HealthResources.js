6501e72bf8393b616309c9a94d4317f8d0641f99
github username <github email address>
2018-04-10 22:58:40 -0700

    removed ul and li

--- src/components/shared/HealthResources.js
+++ src/components/shared/HealthResources.js
@@ -5,12 +5,7 @@ export default () => {
     <div className="bodyContainer">
       <div className="App">
         <h1 className="App-title">Health Resources</h1>
-        <p className="App-intro">
-          <ul>
-            <li href="https://www.hopkinsmedicine.org/healthlibrary/conditions/adult/mental_health_disorders/topic_index_-_mental_health_disorders_85,P00763">John Hopkins Medicine</li>
-            <li href="https://www.mentalhelp.net/articles/depression-hotline/">Mental Help</li>
-          </ul>
-        </p>
+        <p className="App-intro">Details Coming Soon</p>
       </div>
     </div>
   )
