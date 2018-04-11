f9fcb59d4ab5b25d0b94fff93f6a04dbcda3d0e3
github username <github email address>
2018-04-11 00:07:29 -0700

    removed return()

--- src/components/users/UserAttr.js
+++ src/components/users/UserAttr.js
@@ -1,23 +1,21 @@
 import React from 'react';
 
-import Details from './Details'
-import Medications from './Medications'
-import Insurances from './Insurances'
-import Providers from './Providers'
+import Details from './users/Details'
+import Medications from './users/Medications'
+import Insurances from './users/Insurances'
+import Providers from './users/Providers'
 
 const UserAttr = (props) => {
-  return (
-    <div className="UserInfo">
-    {props.users.map(user =>
-      <div className="UserAttributes" key={user.id}>
-        <Details user={user}/>
-        <Medications user={user}/>
-        <Insurances user={user}/>
-        <Providers user={user}/>
-      </div>
-    )}
+  <div className="UserInfo">
+  {props.users.map(user =>
+    <div className="UserAttributes" key={user.id}>
+      <Details user={user}/>
+      <Medications user={user}/>
+      <Insurances user={user}/>
+      <Providers user={user}/>
     </div>
-  )
+  )}
+  </div>
 };
 
 export default UserAttr;
