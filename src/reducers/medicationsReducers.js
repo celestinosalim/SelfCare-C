dd5b79c32bcf510e41838808626cda530c3089fe
github username <github email address>
2018-04-11 16:06:59 -0700

    updated add_medication action; removed comments

--- src/reducers/medicationsReducers.js
+++ src/reducers/medicationsReducers.js
@@ -8,51 +8,17 @@ export default (state = [], action) => {
       return action.medications;
 
     case types.ADD_MEDICATION:
-      return state.concat(action.medication);
+      id++;
+      const medication = Object.assign({}, action.medication, { id: id });
+      return state.concat(medication);
 
     // case types.UPDATE_MEDICATION:
     //   return state.concat(action.medication);
 
     case types.DELETE_MEDICATION:
-      return state.filter(medications => medications.id !== action.id)
+      return state.filter(medication => medication.id !== action.id);
+
     default:
       return state;
   }
 }
-
-//Testing reducer
-// const medicationsReducer = (state = [], action) => {
-//   switch (action.type) {
-//     case "REQUEST_MEDICATIONS":
-//       return action.medications;
-//     default:
-//       return state;
-//   }
-// }
-
-// medications = medicationsReducer(undefined, {
-//   type: '@@INIT'
-// })
-
-// medications = medicationsReducer(undefined, {
-//   type: 'REQUEST_MEDICATIONS',
-//   medications: { name: 'med c' }
-// })
-//
-// medications = medicationsReducer(undefined, {
-//   type: 'REQUEST_MEDICATIONS',
-//   medications: [{ name: 'med c' }, { name: 'med d' }]
-// })
-
-// const medications = (state = [], action) => {
-//   switch (action.type) {
-//     case "REQUEST_MEDICATIONS":
-//       return action.medications;
-//     default:
-//       return state;
-//   }
-// }
-
-// const reducers = combineReducers({
-//   medications: medications
-// })
