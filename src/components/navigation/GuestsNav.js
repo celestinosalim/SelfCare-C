c05a033ade94babe16978a80499d088e8b1c6763
github username <github email address>
2018-04-11 00:09:42 -0700

    removed component; turned to const; added navbar className

--- src/components/navigation/GuestsNav.js
+++ src/components/navigation/GuestsNav.js
@@ -1,51 +1,75 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom'
 
 const link = {
   width: '100px',
   padding: '12px',
   margin: '0 6px 6px',
-  // background: 'blue',
   textDecoration: 'none',
-  // color: 'white',
 }
 
-class GuestsNavbar extends Component {
-  render() {
-    return (
-      <div>
-        <NavLink
-          to="/"
-          style={link}
-          >Home
-        </NavLink>
+const GuestsNavbar = () =>
+  <div className="navbar">
+    <NavLink
+      to="/"
+      style={link}
+      >Home
+    </NavLink>
 
-        <NavLink
-          to="/about"
-          style={link}
-          >About
-        </NavLink>
+    <NavLink
+      to="/about"
+      style={link}
+      >About
+    </NavLink>
 
-        <NavLink
-          to="/health_resources"
-          style={link}
-          >Health Resources
-        </NavLink>
+    <NavLink
+      to="/health_resources"
+      style={link}
+      >Health Resources
+    </NavLink>
 
-        <NavLink
-          to="/login"
-          style={link}
-          >Log In
-        </NavLink>
+    <NavLink
+      to="/login"
+      style={link}
+      >Log In
+    </NavLink>
 
-        <NavLink
-          to="/signup"
-          style={link}
-          >Sign Up
-        </NavLink>
-      </div>
-    )
-  }
-}
+    <NavLink
+      to="/signup"
+      style={link}
+      >Sign Up
+    </NavLink>
+
+
+    <NavLink
+      to="/entries"
+      style={link}
+      >Entries
+    </NavLink>
+
+    <NavLink
+      to="/health_resources"
+      style={link}
+      >Health Resources
+    </NavLink>
+
+    <NavLink
+      to="/user_profile"
+      style={link}
+      >Your Profile
+    </NavLink>
+
+    <NavLink
+      to="#"
+      style={link}
+      >Account Settings
+    </NavLink>
+
+    <NavLink
+      to="/"
+      style={link}
+      >Log Out
+    </NavLink>
+  </div>;
 
 export default GuestsNavbar
--- src/components/navigation/UsersNav.js
+++ src/components/navigation/UsersNav.js
@@ -1,51 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom'
 
 const link = {
   width: '100px',
   padding: '12px',
   margin: '0 6px 6px',
-  // background: 'blue',
   textDecoration: 'none',
-  // color: 'white',
 }
 
-class UsersNav extends Component {
-  render() {
-    return (
-      <div>
-        <NavLink
-          to="/entries"
-          style={link}
-          >Entries
-        </NavLink>
+const UsersNav = () =>
+  <div className="navbar">
+    <NavLink
+      to="/entries"
+      style={link}
+      >Entries
+    </NavLink>
 
-        <NavLink
-          to="/health_resources"
-          style={link}
-          >Health Resources
-        </NavLink>
+    <NavLink
+      to="/health_resources"
+      style={link}
+      >Health Resources
+    </NavLink>
 
-        <NavLink
-          to="/user_profile"
-          style={link}
-          >Your Profile
-        </NavLink>
+    <NavLink
+      to="/user_profile"
+      style={link}
+      >Your Profile
+    </NavLink>
 
-        <NavLink
-          to="#"
-          style={link}
-          >Account Settings
-        </NavLink>
+    <NavLink
+      to="#"
+      style={link}
+      >Account Settings
+    </NavLink>
 
-        <NavLink
-          to="/"
-          style={link}
-          >Log Out
-        </NavLink>
-      </div>
-    )
-  }
-}
+    <NavLink
+      to="/"
+      style={link}
+      >Log Out
+    </NavLink>
+  </div>;
 
 export default UsersNav
