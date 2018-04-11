31c5d311fcf363ce7f2112b1fe7708221a7a562c
github username <github email address>
2018-04-11 12:00:10 -0700

    moved speciality and first visit column after phone, updated the provider departments as a mapped collection

--- src/components/users/Providers.js
+++ src/components/users/Providers.js
@@ -5,11 +5,13 @@ const Providers = ({providers}) => {
 
   const renderProviders = providers.map(provider =>
     <tr className='AttrInfo' key={provider.id}>
-      <td>provider department</td>
-      <td>{provider.first_visit}</td>
       <td>{provider.name}</td>
       <td>{provider.address}</td>
       <td>{provider.phone}</td>
+      {provider.departments.map(department =>
+        <td>{department.name}</td>
+      )}
+      <td>{provider.first_visit}</td>
       <td>{provider.notes}</td>
       <td><Button bsStyle="link">Edit</Button> | <Button bsStyle="link">Delete</Button></td>
     </tr>
@@ -19,11 +21,11 @@ const Providers = ({providers}) => {
     <Table striped bordered condensed hover>
       <thead>
         <tr>
-          <th>Speciality</th>
-          <th>First Visit</th>
           <th>Provider Name</th>
           <th>Address</th>
           <th>Phone</th>
+          <th>Speciality</th>
+          <th>First Visit</th>
           <th>Notes</th>
           <th>Edit / Delete</th>
         </tr>
