bf45a474af7bb7fe0d255892b418119e193a7fa1
github username <github email address>
2018-04-10 20:16:11 -0700

    removed Component text

--- src/components/users/UserProfile.js
+++ src/components/users/UserProfile.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Image, Grid, Col, Button} from 'react-bootstrap';
 
 import UserDetails from '../../container/users/UserDetails'
