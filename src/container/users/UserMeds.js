52ce714dbcce8f73c6a21a2af03f59c4142e7ad0
github username <github email address>
2018-04-13 14:26:16 -0700

    moved addNewObj functionality to Medications Component

--- src/container/users/UserMeds.js
+++ src/container/users/UserMeds.js
@@ -1,43 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button } from 'react-bootstrap';
 import { getMedications } from '../../actions/medicationActions';
-import Medications from '../../components/users/Medications';
-import MedicationForm from '../../container/forms/MedicationForm';
+import Medications from './Medications';
 
 class UserMeds extends Component {
-  constructor(){
-    super();
-    this.state = {
-      isHidden: true
-    }
-  }
-
-  toggleHidden(){
-    this.setState({
-      isHidden: !this.state.isHidden
-    })
-  }
-
   componentDidMount() {
     this.props.getMedications()
   }
 
   render(){
-    const AddNewObjBtn = (
-      <div className="AttrNew">
-        <Button onClick={this.toggleHidden.bind(this)}>Add New Medication</Button>
-        {!this.state.isHidden && <MedicationForm />}
-      </div>
-    )
-
     return (
       <div className="UserMedications">
         <div className="AttrTitle">
           <h3>Medication</h3>
         </div>
         <Medications medications={this.props.medications}/>
-        {AddNewObjBtn}
       </div>
     )
   }
