117cb1cf52ee86d79f693b5406f88d8ca8ac8596
github username <github email address>
2018-04-12 22:50:39 -0700

    moved code from addMedication and editMedication form appearance upon clicking on Add New Medication Button and Edit Button

--- src/components/users/Medications.js
+++ src/components/users/Medications.js
@@ -1,45 +1,83 @@
 import React, { Component } from 'react';
 import { Button, Table } from 'react-bootstrap';
-import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { deleteMedication } from '../../actions/medicationActions';
 
-import EditMedication from '../../container/editMedication'
+import MedicationForm from '../../container/forms/MedicationForm'
 
-const Medications = ({medications}) =>  {
+class Medications extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isHidden: true
+    }
+    this.toggleEdit = this.toggleEdit.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+  }
 
-  const emptyMessage = (
-    <tr>
-      <td colSpan="6">There are no medications in your list</td>
-    </tr>
-  )
+  toggleEdit(){
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
+  }
 
-  const renderMeds = medications.map(medication =>
-    <tr className='AttrInfo' key={medication.id}>
-      <td>{medication.name}</td>
-      <td>{medication.dose}</td>
-      <td>{medication.first_dose}</td>
-      <td>{medication.prescribed}</td>
-      <td>{medication.notes}</td>
-      <EditMedication medication={medication} id={medication.id}/>
-    </tr>
-  )
+  handleDelete(){
+    this.props.deleteMedication(this.props.id);
+  }
 
-  return (
-    <Table striped bordered condensed hover>
-      <thead>
-        <tr>
-          <th>Medication Name</th>
-          <th>Dose</th>
-          <th>Date of First Dose</th>
-          <th>Prescribed By</th>
-          <th>Notes</th>
-          <th>Edit / Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        {renderMeds.length === 0 ? emptyMessage : renderMeds}
-      </tbody>
-    </Table>
-  )
+  render(){
+    const emptyMessage = (
+      <tr>
+        <td colSpan="6">There are no medications in your list</td>
+      </tr>
+    )
+
+    const renderMeds = this.props.medications.map(medication =>
+      <tr className='AttrInfo' key={medication.id}>
+        <td>{medication.name}</td>
+        <td>{medication.dose}</td>
+        <td>{medication.first_dose}</td>
+        <td>{medication.prescribed}</td>
+        <td>{medication.notes}</td>
+        <td><Button bsStyle="link" onClick={this.toggleEdit}>Edit</Button> | <Button bsStyle="link" onClick={this.handleDelete}>Delete</Button></td>
+      </tr>
+    )
+
+    return (
+      <div className="AttrList">
+        <Table striped bordered condensed hover>
+          <thead>
+            <tr>
+              <th>Medication Name</th>
+              <th>Dose</th>
+              <th>Date of First Dose</th>
+              <th>Prescribed By</th>
+              <th>Notes</th>
+              <th>Edit / Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {renderMeds.length === 0 ? emptyMessage : renderMeds}
+          </tbody>
+        </Table>
+          {!this.state.isHidden && <MedicationForm />}
+        <br />
+      </div>
+    )
+  }
 }
 
-export default Medications;
+const mapStateToProps = (state) => {
+  return ({
+    medication: state.medication
+  });
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    deleteMedication: deleteMedication
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Medications);
--- src/container/users/UserMeds.js
+++ src/container/users/UserMeds.js
@@ -1,24 +1,43 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
 import { getMedications } from '../../actions/medicationActions';
 import Medications from '../../components/users/Medications';
-import AddMedication from '../addMedication';
+import MedicationForm from '../../container/forms/MedicationForm';
 
 class UserMeds extends Component {
+  constructor(){
+    super();
+    this.state = {
+      isHidden: true
+    }
+  }
+
+  toggleHidden(){
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
+  }
+
   componentDidMount() {
     this.props.getMedications()
   }
 
   render(){
+    const AddNewObjBtn = (
+      <div className="AttrNew">
+        <Button onClick={this.toggleHidden.bind(this)}>Add New Medication</Button>
+        {!this.state.isHidden && <MedicationForm />}
+      </div>
+    )
+
     return (
       <div className="UserMedications">
         <div className="AttrTitle">
           <h3>Medication</h3>
         </div>
-        <div className="AttrList">
-          <Medications medications={this.props.medications}/>
-        </div>
-        <AddMedication />
+        <Medications medications={this.props.medications}/>
+        {AddNewObjBtn}
       </div>
     )
   }
