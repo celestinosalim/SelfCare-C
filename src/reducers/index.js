062ca8b878a6903373b97e56cd5c186520a91180
github username <github email address>
2018-04-18 17:16:28 -0700

    removed FormDataReducers files as they/re no longer needed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,16 @@
 import { combineReducers } from 'redux';
 import authReducers from './authReducers';
 import medicationsReducers from './medicationsReducers';
-import medicationFormDataReducers from './medicationFormDataReducers';
 import insurancesReducers from './insurancesReducers';
-import insuranceFormDataReducers from './insuranceFormDataReducers';
 import providersReducers from './providersReducers';
-import providerFormDataReducers from './providerFormDataReducers';
 import behaviorsReducers from './behaviorsReducers';
 // import conditionReducers from './conditionsReducers';
 
 const rootReducer = combineReducers({
   auth: authReducers,
   medications: medicationsReducers,
-  medicationFormData: medicationFormDataReducers,
   insurances: insurancesReducers,
-  insuranceFormData: insuranceFormDataReducers,
   providers: providersReducers,
-  providerFormData: providerFormDataReducers,
   behaviors: behaviorsReducers,
   // condition: conditionReducers
 });
