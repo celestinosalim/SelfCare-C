89c143ef792bcdbb5c784508eacadf29a7584af8
github username <github email address>
2018-08-14 10:59:02 -0700

    added button from bootstrap

--- src/components/shared/LandingPage.js
+++ src/components/shared/LandingPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Form, Button, Col, Grid } from 'react-bootstrap';
 
 export default () => {
   return (
     <div className="container">
       <h1 className="App-title">Self-Care Tracker</h1>
       <p className="App-intro">Manage your <strong>Mental Health and Emotions</strong> with your daily habits.</p>
-      <button bsStyle="primary">Learn more</button>
+      <Button bsStyle="primary">Learn more</Button>
     </div>
   )
 }
