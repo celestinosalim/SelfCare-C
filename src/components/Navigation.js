f40e082e09505202bc3b64d2337e4e81fb31a7a0
github username <github email address>
2018-08-14 09:26:46 -0700

    moved Navigation out of shared directory

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import { Link, withRouter } from "react-router-dom";
-// import { logout } from '../actions/authActions';
+import { logout } from '../actions/authActions';
 
 class Navigation extends Component {
-  // handleLogout = (e) => {
-  //   e.preventDefault();
-  //   this.props.logout();
-  //   this.props.history.push('/')
-  // }
+  handleLogout = (e) => {
+    e.preventDefault();
+    this.props.logout();
+    this.props.history.push('/')
+  }
 
   render () {
     const logoLink = (
@@ -19,7 +19,7 @@ class Navigation extends Component {
 
     const mainNav = (
       <nav>
-        <ul id="menu">
+        <ul>
           <Link to="/"><li>{logoLink}</li></Link>
           <Link to="/about"><li>About</li></Link>
           <Link to="/health_resources"><li>Health Resources</li></Link>
@@ -31,22 +31,20 @@ class Navigation extends Component {
 
     const userNav = (
       <nav>
-        <ul id="menu">
+        <ul>
           <Link to="/"><li>{logoLink}</li></Link>
           <Link to="/entries"><li>Entries</li></Link>
           <Link to="/health_resources"><li>Health Resources</li></Link>
           <Link to="/user_profile"><li>Profile</li></Link>
-          <button><li>Log Out</li></button>
-          {/* <button onClick={(e) => this.handleLogout(e)} ><li>Log Out</li></button> */}
+          <button onClick={(e) => this.handleLogout(e)} ><li>Log Out</li></button>
         </ul>
       </nav>
     );
 
     return (
-      <div>{this.props.isAuthenticated ? mainNav : userNav}</div>
+      <div>{this.props.isAuthenticated ? userNav : mainNav}</div>
     )
   }
 }
 
-export default Navigation
-// export default Navigation = withRouter(connect(null, {logout})(Navigation));
+export default Navigation = withRouter(connect(null, {logout})(Navigation));
