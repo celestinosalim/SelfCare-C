89fd02a7e450698161e859029fa58f052e36f0f8
github username <github email address>
2018-10-12 21:00:52 -0700

    wrapped hero detail div with main element

--- src/components/shared/LandingPage.js
+++ src/components/shared/LandingPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 
 export default () => {
   return (
-    <div className="container">
-      <h1 className="App-title">Self-Care Tracker</h1>
-      <p className="App-intro">Manage your <strong>Mental Health and Emotions</strong> with your daily habits.</p>
-      <Button bsStyle="primary">Learn more</Button>
-    </div>
+    <main>
+      <div className="hero-details">
+        <h1 className="App-title">Mental Health Symptom Management</h1>
+        <p className="App-intro">Manage your Mental Health and Emotions with your daily habits.</p>
+        <button>Learn more</button>
+      </div>
+    </main>
   )
 }
