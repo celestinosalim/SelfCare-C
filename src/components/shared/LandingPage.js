95066adc97858f941d9a474f041e37b9c7f7734e
github username <github email address>
2018-10-15 14:24:37 -0700

    updated content

--- src/components/shared/LandingPage.js
+++ src/components/shared/LandingPage.js
@@ -5,7 +5,7 @@ export default () => {
     <main>
       <div className="hero-details">
         <h1 className="App-title">Mental Health Symptom Management</h1>
-        <p className="App-intro">Manage your Mental Health and Emotions with your daily habits.</p>
+        <p className="App-intro">Manage your Mental Health with your daily habits.</p>
         <button>Learn more</button>
       </div>
     </main>
--- src/container/About.js
+++ src/container/About.js
@@ -10,11 +10,15 @@ class About extends Component  {
 
   render() {
     return (
-      <div className="container">
-        <h1 className="page-title">About Self-Care</h1>
-        <p>Self-Care tracks daily habits that can help manage symptoms for those affect by Mental Health. Tracking these daily routines and habits can help inform the user on possible trigger points and mood changes. Setting a daily routine and creating a pattern of behaviors can help improve symptoms in hopes for a brighter future. Below are some of the habits and routines we include for tracking.</p>
-        <Behaviors behaviors={this.props.behaviors} />
-      </div>
+      <main>
+        <div className="app-details">
+          <h1 className="page-title">Change the relationship you have with yourself for the better.</h1>
+          <p>SelfCare is a simple and organized way for you to learn more about your mental health. Share with partners, friends, family, and medical providers to reshape society's view about mental health.</p>
+          <p>Tracks daily habits from sleep quality to moods in search of patterns to help determine possible trigger points and mood changes. Setting a daily routine and creating a pattern of behaviors can help improve symptoms in hopes for a brighter future.</p>
+          <p>Below are some of the habits and routines we include for tracking.</p>
+          <Behaviors behaviors={this.props.behaviors} />
+        </div>
+      </main>
     )
   }
 };
